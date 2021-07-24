d4485198dee98c269484253dd613410a603c3b76
SayemAhmedSayem <sayemahmed92@gmail.com>
2021-07-24 22:54:32 +0600

    initial commit

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>P Learning</title>
+		<style>
+			.phpcodding{width:900px; margin: 0 auto; background:#ddd;}
+			.headeroption, .footeroption {background: #444;color: #fff; text-align:center; padding:20px;}
+			.headeroption h2, .footeroption h2{margin:0;}
+			.maincontent{min-height:400px; padding:20px;}
+		</style>
+	</head>
+
+	<body>
+	<div class="phpcodding">
+		<section class="headeroption">
+			<h2>This is Header</h2>
+		</section>
+		<section class="maincontent">
+			<?php
+				echo "Sayem Ahmed Sayem";
+			?>
+		</section>
+		<section class="footeroption">
+			<h2>This is Footer</h2>
+		</section>
+	</div>
+	</body>
+</html>
